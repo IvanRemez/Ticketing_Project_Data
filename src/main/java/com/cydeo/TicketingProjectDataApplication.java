9a2695a640309e7782a1b73457553db0d301fe70
Ivan_Cydeo <remezivan1@gmail.com>
2023-04-18 19:05:00 -0400

    part-1 done

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,24 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication  // includes @Configuration
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
+    }
+
+// REVIEW - add Bean in the container through @Bean annotation:
+    // create class annotated with @Configuration
+    // write a method which returns Object which you are trying to add to container
+    // annotate method with @Bean
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
     }
 
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,37 +1,47 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Service  //like @Component - creates bean
-public class RoleServiceImpl extends AbstractMapService<RoleDTO,Long> implements RoleService {
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
 
     @Override
-    public RoleDTO save(RoleDTO role) {
-        return super.save(role.getId(),role);
+    public List<RoleDTO> listAllRoles() {
+// REVIEW:
+    // Controller requests all RoleDTOs, so it can show in the UI
+    // Need to access DB to get all Roles from the Table
+    // go to Repo and find Service which gives me Roles from DB
+    // inject this Service (RoleRepository)
+
+        List<Role> roleList = roleRepository.findAll();
+
+    // need to convert Role Entities to RoleDTOs
+    // need to use ModelMapper methods
+    // RoleMapper class created for these conversion methods
+    // ^^ class injected here
+
+        return roleList.stream()
+                .map(roleMapper::converToDto).collect(Collectors.toList());
     }
 
     @Override
     public RoleDTO findById(Long id) {
-        return super.findById(id);
+        return null;
     }
-
-    @Override
-    public List<RoleDTO> findAll() {
-        return super.findAll();
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
-    }
-
-    @Override
-    public void update(RoleDTO object) {
-        super.update(object.getId(), object);
-    }
-
 }
